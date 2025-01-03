b6cc5e1036b17df0095d853165b8a940c1e24485
mrWor1d <davyneil53@gmail.com>
2025-01-03 23:57:37 +0100

    added socials login pages

---
+++ ESP32_Server/ESP32_Server.ino
@@ -0,0 +1,19 @@
+#include <FakeAPlib.h>
+#include <WiFi.h>
+#include <DNSServer>
+
+
+DNSServer dnsserv;
+IPAddress apIp(192,168,1,3);
+const char* hostname = "ESP32.AP";
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
